0f51170fe0a10a2143064354b75fc2f8535800e2
Jikku Jose <jikkujose@gmail.com>
2018-06-20 18:22:57 +0530

    Add simple page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
+import "tachyons"
 
 const Layout = ({ children, data }) => (
   <div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,19 @@ import React from "react"
 import Link from "gatsby-link"
 
 const IndexPage = () => (
-  <div>
-    <h1>Qucentis</h1>
-    <p>This is a simple SSR based site</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div className="ph5">
+    <div className="SC:header bg-blue pa3 ph4 white f4 code b">Qucentis</div>
+    <div className="SC:body bg-white pa3 ph4 black-60 f4 code measure-wide">
+      We are a team of 3. We are passionate about blockchain, deep learning &
+      UX. Helping companies tackle their hard tech problems is our living.
+    </div>
+    <div className="SC:body bg-white pa3 ph4 black-60 f4 code measure-wide">
+      Discuss your tech issue with us, lets see whether we can suggest a
+      solution.
+    </div>
+    <div className="SC:body bg-white pa3 ph4 black-60 f4 code measure-wide b">
+      hello@qucentis.com
+    </div>
   </div>
 )
 
