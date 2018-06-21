0bc1c08209d84db029372d793d7c982c67226b47
Jikku Jose <jikkujose@gmail.com>
2018-06-21 10:13:12 +0530

    Update text

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,22 @@ import React from "react"
 import Link from "gatsby-link"
 
 const IndexPage = () => (
-  <div className="ph5">
-    <div className="SC:header bg-blue pa3 ph4 white f4 code b">Qucentis</div>
-    <div className="SC:body bg-white pa3 ph4 black-60 f4 code measure-wide">
-      We are a team of 3. We are passionate about blockchain, deep learning &
-      UX. Helping companies tackle their hard tech problems is our living.
+  <div className="pa5-l">
+    <div className="SC:header bg-blue pa3 ph4-l mb3-l white f4 code b br2-l">
+      Qucentis
     </div>
-    <div className="SC:body bg-white pa3 ph4 black-60 f4 code measure-wide">
+    <div className="SC:body bg-white pa3 ph4-l black-60 f4 code measure-wide-l">
+      Nikhil Mohan, Rakesh BS & Jikku Jose is Qucentis.
+    </div>
+    <div className="SC:body bg-white pa3 ph4-l black-60 f4 code measure-wide-l">
+      We are passionate about blockchain, deep learning & UX. We help businesses
+      tackle their hard tech problems.
+    </div>
+    <div className="SC:body bg-white pa3 ph4-l black-60 f4 code measure-wide">
       Discuss your tech issue with us, lets see whether we can suggest a
       solution.
     </div>
-    <div className="SC:body bg-white pa3 ph4 black-60 f4 code measure-wide b">
+    <div className="SC:body bg-white pa3 ph4-l black-60 f4 code measure-wide b">
       hello@qucentis.com
     </div>
   </div>
